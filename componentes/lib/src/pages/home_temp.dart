e50aa6dde31f186ad8aab7e5c8e869de82e7d08a
Carvajal <carlos.carvajal@dxc.com>
2020-11-23 17:36:01 -0600

    video 66

---
+++ componentes/lib/src/pages/home_temp.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(
+        children: _crearItemsCorto(),
+      ),
+      // body: ListView(
+      //   children: _crearItems(),
+      // ),
+    );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = new List<Widget>();
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      lista..add(tempWidget)..add(Divider());
+    }
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorto() {
+    return opciones.map((item) {
+      return Column(
+        children: [
+          ListTile(
+            title: Text(item),
+            subtitle: Text('Cualquier cosa'),
+            leading: Icon(Icons.account_balance_wallet), //widget al inicio
+            trailing: Icon(Icons.keyboard_arrow_right), //widget al final
+            onTap: () {},
+          ),
+          Divider(),
+        ],
+      );
+    }).toList();
+  }
+}
